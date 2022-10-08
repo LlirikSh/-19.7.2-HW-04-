d7718f3ff3d3e2773fc4176f9d640c7bd0afb45a
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-10-08 20:59:25 +0300

    app.py-file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class PetFriends:
     def __init__(self):
         self.base_url = 'https://petfriends.skillfactory.ru/'
 
-    def get_app_key(self, email, password):
+    def get_app_key(self, email: str, password: str) -> json:
         '''Метод делает get запрос к API  сервера с емейлом и паролем
          в header запроса и возвращает код статуса запроса и секретный
           ключ для в формате json либо виде строки'''
@@ -25,7 +25,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def get_list_of_pets(self, auth_key, filter):
+    def get_list_of_pets(self, auth_key: json, filter: str) -> json:
         '''Метод делает get запрос к API сервера с секретным ключом в headers и
           пустым  значением в filter и возвращает: код статуса запроса и список
            всех питомцев в формате Json либо в виде строки'''
@@ -42,7 +42,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def add_new_pets(self, auth_key, name, animal_type, age, pet_photo):
+    def add_new_pets(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
         '''Метод делает post запрос к API сервера, добавляет данные data на сайт PetFriends и
         возвращает код статуса запроса результат в формате json с информацией о животном. '''
         data = MultipartEncoder(
@@ -68,7 +68,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def delete_pets(self, auth_key, pet_id):
+    def delete_pets(self, auth_key: json, pet_id: str) -> json:
         '''Метод делает delete запрос к API сервера, удоляет питомца по его ID и возвращает
         статус запроса'''
         headers = {
@@ -86,7 +86,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def update_pet_info(self, auth_key, pet_id, name, animal_type, age):
+    def update_pet_info(self, auth_key: json, pet_id: str, name: str, animal_type: str, age: str) ->json:
         '''Метод делает put запррс к API сервера изменяет данные питомца и возвращает код статуса запроса и
         измененные данные в формате json'''
         headers = {
@@ -109,7 +109,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def add_new_pet_without_photo(self, auth_key, name, animal_type, age):
+    def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) ->json:
         '''Метод делает post запрос к API сервера, добавляет новые данные из data на сайт
         и возвращает код статуса запроса и результат в формате json с информацией о животном.'''
         headers = {
@@ -131,7 +131,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-    def add_photo_of_pet(self, auth_key, pet_id, pet_photo):
+    def add_photo_of_pet(self, auth_key: json, pet_id: str, pet_photo: str) ->json:
         '''Метод делает post запрос к API сервера и добавляет новое фото указанного
         pet_id питомца. Возвращает код статуса запроса и результат в формате json с информацией о животном.'''
         data = MultipartEncoder(
@@ -152,4 +152,3 @@ class PetFriends:
         except:
             result = res.text
         return status, result
-
